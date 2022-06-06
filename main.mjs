014cd86cf18609a4208ffa73634c5e7afc5ff6c1
bielTsilva <gelogabriel@gmail.com>
2022-06-05 21:06:39 -0300

    Resolução de atividade sobre módulos

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Gabriel", 25));
+console.log(mostraCidade("Gabriel", "São Paulo"));
+console.log(mostraHobby("Gabriel", "Guitarra"));
